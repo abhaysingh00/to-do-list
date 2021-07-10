35de99ac9bf8b6770b554fccd27b8510a2d2be25
abhay_singh <76041034+abhaysingh00@users.noreply.github.com>
2021-07-10 12:50:19 +0530

    Add files via upload
    added checkbox and cross

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ let addBtn=document.getElementById("add-btn");
 let task =document.getElementById("task");
 let list=document.getElementById("list");
 addBtn.addEventListener("click", function(){
-let para =document.createElement("p");
-para.classList.add("para");
-para.innerText=task.value;
-list.appendChild(para);
+ 
+let listEle =document.createElement("li"); 
+let checkbox=document.createElement("input");
+let cross=document.createElement("span");
+cross.id="cross";
+cross.textContent="+" ;
+cross.classList.add("cross");
+checkbox.type="checkbox";
+checkbox.name="toDo"; 
+checkbox.id="toDo";
+listEle.append(checkbox);
+listEle.classList.add("listEle");
+listEle.append(task.value);
+listEle.append(cross);
+list.appendChild(listEle);
 task.value="";
-para.addEventListener("click",function(){
-     list.removeChild(para);
+
+    checkbox.addEventListener("click", function(){
+         listEle.style.textDecoration="line-through";
+         listEle.classList.add("afftercb");
+    })
+    cross.addEventListener("click", function(){
+         list.removeChild(listEle);
     })
     })
