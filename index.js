6d01053370bae7ddcd30b7dda614ad2c4b52e38b
abhay_singh <76041034+abhaysingh00@users.noreply.github.com>
2021-07-11 12:52:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 let addBtn=document.getElementById("add-btn");
 let task =document.getElementById("task");
 let list=document.getElementById("list");
+let countEle =document.getElementById("COUNT");
+
+let count=0;
 addBtn.addEventListener("click", function(){
+     count =count +1;
+     if(count<=1){
+          countEle.innerText="TASK PENDING = "+count;}
+          else {
+          countEle.innerText="TASKS PENDING = "+count;}
  
 let listEle =document.createElement("li"); 
 let checkbox=document.createElement("input");
 let cross=document.createElement("span");
+
+
 cross.id="cross";
 cross.textContent="+" ;
 cross.classList.add("cross");
@@ -22,6 +32,11 @@ task.value="";
     checkbox.addEventListener("click", function(){
          listEle.style.textDecoration="line-through";
          listEle.classList.add("afftercb");
+         count = count - 1;
+         if(count<=1){
+         countEle.innerText="TASK PENDING = "+count;}
+         else {
+         countEle.innerText="TASKS PENDING = "+count;}
     })
     cross.addEventListener("click", function(){
          list.removeChild(listEle);
